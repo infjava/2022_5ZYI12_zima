e7f898f96d45516952087e59a79db9883d573fcc
Jan Janech <jan.janech@fri.uniza.sk>
2022-10-04 08:08:05 +0200

    03 projekt pre kalkulacku

---
+++ 03 kalkulacka/kalkulacka/Kalkulacka.java
@@ -0,0 +1,18 @@
+public class Kalkulacka {
+    
+    public Kalkulacka() {
+        
+    }
+    
+    public void vykonaj(String operator, int operand) {
+        
+    }
+    
+    public int getVysledok() {
+        
+    }
+    
+    public void vylozOperand(int operand) {
+        
+    }
+}
