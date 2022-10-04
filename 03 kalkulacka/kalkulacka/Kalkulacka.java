bbb008737fcad9f61ae68939a61fd441abc61d6a
Jan Janech <jan.janech@fri.uniza.sk>
2022-10-04 08:22:54 +0200

    03 implementacia kalkulacky

--- 03 kalkulacka/kalkulacka/Kalkulacka.java
+++ 03 kalkulacka/kalkulacka/Kalkulacka.java
@@ -1,18 +1,32 @@
 public class Kalkulacka {
+    private int vysledok;
     
     public Kalkulacka() {
-        
+        this.vysledok = 0;
     }
     
     public void vykonaj(String operator, int operand) {
-        
+        switch (operator) {
+            case "+":
+                this.vysledok = this.vysledok + operand;
+                break;
+            case "-":
+                this.vysledok = this.vysledok - operand;
+                break;
+            case "*":
+                this.vysledok = this.vysledok * operand;
+                break;
+            case "/":
+                this.vysledok = this.vysledok / operand;
+                break;
+        }
     }
     
     public int getVysledok() {
-        
+        return this.vysledok;
     }
     
-    public void vylozOperand(int operand) {
-        
+    public void vlozOperand(int operand) {
+        this.vysledok = operand;
     }
 }
