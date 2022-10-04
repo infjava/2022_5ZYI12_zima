d34cd6524bc97df91fb211173dcbcfb04fcbd295
Jan Janech <jan.janech@fri.uniza.sk>
2022-10-04 08:57:49 +0200

    03 infixova kalkulacka

--- 03 kalkulacka/kalkulacka/Kalkulacka.java
+++ 03 kalkulacka/kalkulacka/Kalkulacka.java
@@ -2,15 +2,17 @@ public class Kalkulacka {
     private int vysledok;
     private int operand;
     private boolean vlozenyPrvyOperand;
+    private String predchadzajuciOperator;
     
     public Kalkulacka() {
         this.vysledok = 0;
         this.operand = 0;
         this.vlozenyPrvyOperand = false;
+        this.predchadzajuciOperator = "";
     }
     
     public void vykonaj(String operator) {
-        switch (operator) {
+        switch (this.predchadzajuciOperator) {
             case "+":
                 this.vysledok = this.vysledok + this.operand;
                 break;
@@ -24,6 +26,7 @@ public class Kalkulacka {
                 this.vysledok = this.vysledok / this.operand;
                 break;
         }
+        this.predchadzajuciOperator = operator;
     }
     
     public int getVysledok() {
