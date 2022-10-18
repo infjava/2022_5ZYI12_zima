3d803e412a0d88dbca44046c7531254ba8bcb2f4
Jan Janech <jan.janech@fri.uniza.sk>
2022-10-18 09:14:08 +0200

    05 - opakovanie stringu v cirkuse stringerto

--- 06 cirkusStringerto/CirkusStringerto.java
+++ 06 cirkusStringerto/CirkusStringerto.java
@@ -4,4 +4,13 @@ public class CirkusStringerto {
     public CirkusStringerto(String retazec) {
         this.retazec = retazec;
     }
+    
+    public String zopakuj(int pocetKrat) {
+        StringBuilder vystup = new StringBuilder();
+        for (int i = 0; i < pocetKrat; i++) {
+            vystup.append(this.retazec);
+        }
+        
+        return vystup.toString();
+    }
 }
