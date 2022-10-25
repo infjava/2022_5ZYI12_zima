c58fde4712dd7a7b3c9fc410f4bbf3b038d6ef78
Jan Janech <jan.janech@fri.uniza.sk>
2022-10-25 09:41:04 +0200

    06 - trieda Hra

---
+++ 07 rpsls/RPSLS/Hra.java
@@ -0,0 +1,18 @@
+public class Hra {
+    private Hrac hrac;
+    private Bot bot;
+    private Score score;
+    private Manazer manazer;
+    
+    public Hra() {
+        this.hrac = new Hrac();
+        this.bot = new Bot();
+        this.score = new Score();
+        this.manazer = new Manazer();
+    }
+    
+    public void spusti() {
+        this.manazer.spravujObjekt(this.hrac);
+        this.manazer.spravujObjekt(this.bot);
+    }
+}
