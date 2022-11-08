6c12e1a6aa60826c0197d19762c9ac7a7ad8e023
Jan Janech <jan.janech@fri.uniza.sk>
2022-11-08 09:01:10 +0100

    07 - body v osobe

--- 08 skupina-osoba/Osoba.java
+++ 08 skupina-osoba/Osoba.java
@@ -1,12 +1,20 @@
+import java.util.ArrayList;
+
 public class Osoba {
     private String meno;
     private String priezvisko;
+    private ArrayList<Double> body;
 
     public Osoba(String meno, String priezvisko) {
         this.meno = meno;
         this.priezvisko = priezvisko;
+        this.body = new ArrayList<Double>();
     }
-
+    
+    public void pridajBody(double pocet) {
+        this.body.add(pocet);
+    }
+    
     public String getMeno() {
         return this.meno;
     }
