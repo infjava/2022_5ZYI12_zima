be326588b4677fc8d321057c129db9cff079ba57
Jan Janech <jan.janech@fri.uniza.sk>
2022-11-15 07:58:11 +0100

    08 - dalsie riesenie zadavania hodnot vektora

--- 09 matice a vektory/Vektor.java
+++ 09 matice a vektory/Vektor.java
@@ -1,7 +1,11 @@
 public class Vektor {
     private int[] vektor;
     
-    public Vektor(int... prvky) {
-        this.vektor = prvky;
+    public Vektor(int pocetRozmerov) {
+        this.vektor = new int[pocetRozmerov];
+    }
+    
+    public void nastavHodnotu(int rozmer, int hodnota) {
+        this.vektor[rozmer - 1] = hodnota;
     }
 }
