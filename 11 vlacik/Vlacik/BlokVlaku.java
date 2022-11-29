e0edd1363a566d11694b8f7d462aaa98d8ccd416
Jan Janech <jan.janech@fri.uniza.sk>
2022-11-29 09:23:01 +0100

    11 - bloky vlaku sa daju spajat a hybu sa naraz

--- 11 vlacik/Vlacik/BlokVlaku.java
+++ 11 vlacik/Vlacik/BlokVlaku.java
@@ -2,6 +2,7 @@ public class BlokVlaku {
     private Pozicia pozicia;
     private final TypBlokuVlaku typ;
     private final PolickovyObrazok obrazok;
+    private BlokVlaku pripojeny;
     
     public BlokVlaku(Pozicia pozicia, TypBlokuVlaku typ) {
         this.pozicia = pozicia;
@@ -9,9 +10,18 @@ public class BlokVlaku {
         this.obrazok = new PolickovyObrazok(typ.getCestaKObrazku());
         this.obrazok.zmenUhol(pozicia.getSmer().getUhol());
         this.obrazok.zmenPoziciu(pozicia.getX(), pozicia.getY());
+        
+        this.pripojeny = null;
+    }
+    
+    public void pripoj(BlokVlaku blok) {
+        this.pripojeny = blok;
     }
     
     public void posun(Smer smer) {
+        if (this.pripojeny != null) {
+            this.pripojeny.posun(this.pozicia.getSmer());
+        }
         this.pozicia = this.pozicia.vytvorNovu(smer);
         this.obrazok.zmenUhol(this.pozicia.getSmer().getUhol());
         this.obrazok.zmenPoziciu(this.pozicia.getX(), this.pozicia.getY());
