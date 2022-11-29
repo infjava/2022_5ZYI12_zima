90e720a2d6a0dc23bcd6ff91c29ae6b7e020106e
Jan Janech <jan.janech@fri.uniza.sk>
2022-11-29 08:55:00 +0100

    11 - PolickovyObrazok pre jednoduchsie ovladanie obrazku

---
+++ 11 vlacik/Vlacik/PolickovyObrazok.java
@@ -0,0 +1,28 @@
+public class PolickovyObrazok {
+    private final Obrazok obrazok;
+    private int x;
+    private int y;
+    
+    public PolickovyObrazok(String cesta) {
+        this.obrazok = new Obrazok(cesta);
+        this.obrazok.posunZvisle(-100);
+        this.obrazok.posunVodorovne(-100);
+        this.obrazok.zobraz();
+        
+        this.x = 0;
+        this.y = 0;
+    }
+    
+    public void zmenUhol(int uhol) {
+        this.obrazok.zmenUhol(uhol);
+    }
+    
+    public void zmenPoziciu(int x, int y) {
+        int zmenaX = x - this.x;
+        int zmenaY = y - this.y;
+        this.obrazok.posunZvisle(zmenaX * 35);
+        this.obrazok.posunVodorovne(zmenaY * 35);
+        this.x = x;
+        this.y = y;
+    }
+}
